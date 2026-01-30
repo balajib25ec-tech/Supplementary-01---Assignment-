87327be65a9c09c8ceb3fc72aedd5bebc4d8945e
balajib25ec-tech <balaji.b_25ec@kgkite.ac.in>
2026-01-30 15:20:31 +0530

    Corrected Files

--- problem_31.py
+++ problem_31.py
@@ -1,7 +1,9 @@
 # Problem 31: Concatenate strings
 # Find and fix the error
 
-first_name = "John"
-age = 25
-message = "My name is " + first_name + " and I am " + age + " years old"
+first_name = str(input("Enter your first name: "))
+age =int(input("Enter your age: "))
+
+message = "My name is " + first_name + " and I am " + str(age) + " years old"
 print(message)
+
